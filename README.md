6291993b6b225a8fae2c7052bfc065d8057f8075
lg <>
2023-06-02 10:31:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# gitbtn README
+
+**just click the button!**
+
+![guide](https://github.com/MrYZhou/gitbtn/raw/main/static/1.png)
---
+++ src/index.js
@@ -0,0 +1,30 @@
+const vscode = require('vscode');
+
+function activate(context) {
+  console.log(11);
+  // 创建一个活动栏按钮
+  const myButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+  myButton.text = "$(star) My Button";
+  myButton.tooltip = "Click to run my command";
+  myButton.command = "myExtension.myCommand";
+  myButton.show();
+  
+
+  // 注册命令
+  const disposable = vscode.commands.registerCommand('myExtension.myCommand', () => {
+  // 执行相应的命令逻辑
+  console.log(111);
+  });
+
+  // 将命令和按钮注册到上下文中，以便在插件被停用时进行清理
+  context.subscriptions.push(myButton, disposable);
+}
+
+function deactivate() {
+  // 执行插件的清理逻辑
+}
+
+module.exports = {
+  activate,
+  deactivate
+};
