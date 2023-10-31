1b8cabcbc5041e024b95243c8dcfbec4b996cb5f
larry <1762861715@qq.com>
2023-10-31 23:53:49 +0800

    树视图

--- src/command/treeContainer.js
+++ src/command/treeContainer.js
@@ -1,10 +1,35 @@
 const vscode = require('vscode');
 const Base = require('../base');
-module.exports = class TreeConTainer extends Base{
+module.exports = class TreeConTainer extends Base {
   registe(context) {
-    
+    const rootPath =
+      vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+        ? vscode.workspace.workspaceFolders[0].uri.fsPath
+        : undefined;
+    // 项目信息数据初始化
+    let projectData = new ProjectDataProvider(rootPath)
+    vscode.window.registerTreeDataProvider(
+      'project', projectData
+    );
+    vscode.commands.registerCommand('myExtension.refreshData', () =>
+      projectData.refresh()
+    );
   }
-  unload(){
+  unload() {
     super.tip('清理树视图')
+  }
+}
+class ProjectDataProvider {
+  // vscode约定方法,名字不可改
+  getTreeItem(element) {
+
+  }
+  // vscode约定方法,名字不可改
+  getChildren(element) {
+
+  }
+  // 额外的自定义方法,名称可随意
+  refresh() {
+
   }
 }
