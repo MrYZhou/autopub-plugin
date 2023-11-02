1e944997166f0d42a9503cfe5256e369cade3fa8
lg <1762861715@qq.com>
2023-11-02 10:49:05 +0800

    修改

--- src/command/treeContainer.js
+++ src/command/treeContainer.js
@@ -1,19 +1,18 @@
 const vscode = require('vscode');
 const Base = require('../base');
-module.exports = class TreeConTainer extends Base {
+import { TreeDataProvider, TreeItem, TreeItemCollapsibleState, ProviderResult, window } from "vscode";
+module.exports = class TreeConTainer extends Base  {
   registe(context) {
-    const rootPath =
-      vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
-        ? vscode.workspace.workspaceFolders[0].uri.fsPath
-        : undefined;
+
     // 项目信息数据初始化
-    let projectData = new ProjectDataProvider(rootPath)
+    let projectData = new ProjectDataProvider()
     vscode.window.registerTreeDataProvider(
       'project', projectData
     );
-    vscode.commands.registerCommand('myExtension.refreshData', () =>
-      projectData.refresh()
-    );
+    vscode.commands.registerCommand('autopub.refreshData', () => {
+      vscode.window.showInformationMessage("open dir:")
+      // projectData.refresh()
+    });
   }
   unload() {
     super.tip('清理树视图')
