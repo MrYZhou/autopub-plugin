85cfa41372584f05fa7961b04c4ffe41447beeeb
lg <1762861715@qq.com>
2023-11-29 15:45:03 +0800

    报错处理

--- src/export.ts
+++ src/export.ts
@@ -1,8 +1,9 @@
+import { Base } from "./base";
 
 const path = require('path');
 
 let url = path.join(__dirname, 'command')
-let files = ['gitJump', 'treeContainer', 'initEnv', 'treeStatusBar']
+let files = [ 'gitJump','treeContainer', 'initEnv', 'treeStatusBar']
 
 let testFiles=['dependentest']
 files = [...files,...testFiles]
@@ -11,8 +12,12 @@ let modules: any[] = []
 let actions: Promise<any>[] = []
 for (let index = 0; index < files.length; index++) {
   actions.push(new Promise((res, rej) => {
-    let module = require(path.join(url,`${files[index]}.js`))
-    res(modules[index] = new module())
+    try{
+      let module = require(path.join(url,`${files[index]}.js`))
+      res(modules[index] = new module())
+    }catch(err){
+      Base.tip(err)
+    }
   }))
 }
 
