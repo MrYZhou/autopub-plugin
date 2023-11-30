cef015f8dd97151b980b05e5ecc925ae5f316a09
lg <1762861715@qq.com>
2023-11-30 16:15:34 +0800

    增加写日志的操作

--- src/base.ts
+++ src/base.ts
@@ -7,20 +7,86 @@ const fs = require('fs')
 
 export class Base {
   
-  static sourceDir: string = path.resolve(__dirname, '..', '..', '..', 'environment')
+  static sourceDir: string = path.resolve(__dirname, '..', 'environment')
   static targetDir: string = path.resolve(os.homedir(), '.autopub')
 
   static init() {
-    this.createLogFile()
-  }
-
-  static createLogFile() {
-    if (!fs.existsSync(this.targetDir)) {
-      Base.createFile(this.targetDir)
+    let isExit = fs.existsSync(this.targetDir);
+    if (!isExit) {
+      this.copyFolder(this.sourceDir, this.targetDir)
     }
+    
   }
-  static createFile(path: string) {
-  }
+  // 复制文件夹
+  static copyFolder = (srcDir: string, tarDir: string, cb?: Function) => {
+    let isExit = fs.existsSync(tarDir);
+    if (!isExit) {
+      fs.mkdirSync(tarDir, 777);
+    }
+    //readdir只能读取基于这个sourceDir下的那一级文件和文件夹
+    fs.readdir(srcDir, function (err: any, files: any[]) {
+      if (err) {
+        return;
+      }
+      files.forEach((file) => {
+        let srcPath = path.join(srcDir, file);
+        let tarPath = path.join(tarDir, file);
+
+        fs.stat(srcPath, (err: any, stats: { isDirectory: () => any }) => {
+          if (stats.isDirectory()) {
+            fs.existsSync(tarPath)
+              ? Base.copyFolder(srcPath, tarPath)
+              : fs.mkdirSync(tarPath, 777) && Base.copyFolder(srcPath, tarPath);
+          } else {
+            Base.copyFile(srcPath, tarPath);
+          }
+        });
+      });
+
+      //为空时直接回调
+      files.length === 0 && cb && cb();
+    });
+  };
+  // 复制文件,前提是文件夹存在
+  static copyFile = function (
+    srcPath: any,
+    tarPath: any,
+    cb?: ((arg0: any) => any) | undefined
+  ) {
+    let rs = fs.createReadStream(srcPath);
+    rs.on("error", function (err: any) {
+      if (err) {
+        console.log("read error", srcPath);
+      }
+      cb && cb(err);
+    });
+
+    let ws = fs.createWriteStream(tarPath);
+    ws.on("error", function (err: any) {
+      if (err) {
+        console.log("write error", tarPath);
+      }
+      cb && cb(err);
+    });
+    ws.on("close", function (ex: any) {
+      cb && cb(ex);
+    });
+
+    rs.pipe(ws);
+  };
+
+  // 初始化文件夹,某个文件路径中间文件夹不存在就生成,只用于文件
+  // 也可以理解创建上一级的目录
+  static mkdirs = (tarDir: string) => {
+    return new Promise((resolve, reject) => {
+      let dir = path.resolve(tarDir, "../");
+      if (tarDir && !fs.existsSync(dir)) {
+        Base.mkdirs(dir);
+        fs.mkdirSync(dir, 777);
+      }
+      resolve(1);
+    });
+  };
   
   static readFile(path: string) {
     const data = fs.readFileSync(path, 'UTF-8').toString()
