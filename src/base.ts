5a263214bd62ff925c826cdbfa944fb33285a68c
lg <1762861715@qq.com>
2023-11-30 09:55:35 +0800

    环境初始化1

--- src/base.ts
+++ src/base.ts
@@ -1,7 +1,32 @@
 
 const config = require('./config')
+
+const path = require('path')
+const os = require('os')
+const fs = require('fs')
+
 export class Base {
   
+  static sourceDir: string = path.resolve(__dirname, '..', '..', '..', 'environment')
+  static targetDir: string = path.resolve(os.homedir(), '.autopub')
+
+  static init() {
+    this.createLogFile()
+  }
+
+  static createLogFile() {
+    if (!fs.existsSync(this.targetDir)) {
+      Base.createFile(this.targetDir)
+    }
+  }
+  static createFile(path: string) {
+  }
+  
+  static readFile(path: string) {
+    const data = fs.readFileSync(path, 'UTF-8').toString()
+    return JSON.parse(data)
+  }
+
   static time(label: string) {
     if (config.test) {
       console.time(label)
--- src/command/initEnv.ts
+++ src/command/initEnv.ts
@@ -3,6 +3,7 @@ import { Base } from "../base";
 module.exports = class TreeStatusBar {
   registe(context: any) {
     Base.tip('环境初始化')
+    Base.init()
     
   }
   deactivate() {
