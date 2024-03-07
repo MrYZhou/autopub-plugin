460cb86389930934fef77bdf3f6d94a057424b6a
lg <1762861715@qq.com>
2024-03-07 14:03:13 +0800

    1

--- packages/extension/src/command/host.ts
+++ packages/extension/src/command/host.ts
@@ -6,36 +6,37 @@ module.exports = class Webview {
   registe(context: any) {
     let openHostAdd = vscode.commands.registerCommand('autopub.host.add', async () => {
       // 创建一个新的 WebView 视图
-    const panel = vscode.window.createWebviewPanel('myWebView', '添加主机',
-    vscode.ViewColumn.One,
-    { enableScripts: true,
-      localResourceRoots: [
-        vscode.Uri.file(path.join(context.extensionPath, 'out/view')),
-      ],
-    }
-  );
+      const panel = vscode.window.createWebviewPanel('myWebView', '添加主机',
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+          localResourceRoots: [
+            vscode.Uri.file(path.join(context.extensionPath, 'out/view')),
+          ],
+        }
+      );
 
-  // 注册消息监听器来接收来自webview的消息
-  panel.webview.onDidReceiveMessage(message => {
-    
-    // 处理来自表单的数据
-    if (message.type === 'form-data') {
-      
-      console.log('Received form data:', message.data);
-      // 这里实现将数据存储到数据库或其他操作
-    }
-  });
+      // 注册消息监听器来接收来自webview的消息
+      panel.webview.onDidReceiveMessage(message => {
 
-  // 在 WebView 视图中加载 HTML 内容
-  panel.webview.html = getWebviewHTML(context.extensionUri);
+        // 处理来自表单的数据
+        if (message.type === 'form-data') {
 
-  // webview销毁函数
-  panel.onDidDispose(() => {
-    console.log('panel onDidDispose')
-  })
+          console.log('Received form data:', message.data);
+          // 这里实现将数据存储到数据库或其他操作
+        }
+      });
+
+      // 在 WebView 视图中加载 HTML 内容
+      panel.webview.html = getWebviewHTML(context.extensionUri);
+
+      // webview销毁函数
+      panel.onDidDispose(() => {
+        console.log('panel onDidDispose')
+      })
     })
     context.subscriptions.push(openHostAdd);
-    
+
 
   }
   deactivate() {
