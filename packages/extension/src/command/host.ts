60ac1708c55d974c116b3800877b9fb2d168ee91
larry <1762861715@qq.com>
2024-06-06 00:50:21 +0800

    消息发送

--- packages/extension/src/command/host.ts
+++ packages/extension/src/command/host.ts
@@ -5,78 +5,80 @@ import { Uri } from 'vscode';
 const path = require('path');
 const fs = require('fs');
 module.exports = class Webview {
-  registe(context: vscode.ExtensionContext) {
-    let extensionUri = vscode.extensions.getExtension('larry.autopub')?.extensionUri;
+    registe(context: vscode.ExtensionContext) {
+        let extensionUri = vscode.extensions.getExtension('larry.autopub')?.extensionUri;
+        let panel: vscode.WebviewPanel
+        let openHostAdd = vscode.commands.registerCommand('autopub.host.add', async () => {
+        // 创建一个新的 WebView 视图
+            panel = vscode.window.createWebviewPanel('myWebView', '添加主机',
+                vscode.ViewColumn.One,
+                {
+                    enableScripts: true,
+                    localResourceRoots: [
+                        vscode.Uri.file(path.join(context.extensionPath, 'out/view')),
+                    ],
+                }
+            );
 
-    let openHostAdd = vscode.commands.registerCommand('autopub.host.add', async () => {
-      // 创建一个新的 WebView 视图
-      const panel = vscode.window.createWebviewPanel('myWebView', '添加主机',
-        vscode.ViewColumn.One,
-        {
-          enableScripts: true,
-          localResourceRoots: [
-            vscode.Uri.file(path.join(context.extensionPath, 'out/view')),
-          ],
-        }
-      );
+            // 注册消息监听器来接收来自webview的消息
+            panel.webview.onDidReceiveMessage(event => {
 
-      // 注册消息监听器来接收来自webview的消息
-      panel.webview.onDidReceiveMessage(message => {
+                // 处理来自表单的数据
+                if (event.type === 'hostAdd') {
 
-        // 处理来自表单的数据
-        if (message.type === 'form-data') {
+                    console.log('Received form data:', event.data);
+                    // 这里实现将数据存储到数据库或其他操作
+                }
+            });
 
-          console.log('Received form data:', message.data);
-          // 这里实现将数据存储到数据库或其他操作
-        }
-      });
+            // 在 WebView 视图中加载 HTML 内容
+            panel.webview.html = await getWebviewHTML(panel, extensionUri ?? Uri.file(''));
 
-      // 在 WebView 视图中加载 HTML 内容
-      panel.webview.html = await  getWebviewHTML(panel, extensionUri ?? Uri.file(''));
-
-      // webview销毁函数
-      panel.onDidDispose(() => {
-        console.log('panel onDidDispose')
-      })
-    })
-    context.subscriptions.push(openHostAdd);
+            // webview销毁函数
+            panel.onDidDispose(() => {
+                console.log('panel onDidDispose')
+            })
 
 
-  }
-  deactivate() {
-  }
+        })
+        context.subscriptions.push(openHostAdd);
+
+
+    }
+    deactivate() {
+    }
 }
 // 获取目录下文件信息
 async function listFilesInDirectory(uri: vscode.Uri): Promise<any> {
-  try {
-      // 使用vscode.workspace.fs.readDirectory来获取目录内容
-      const entries = await vscode.workspace.fs.readDirectory(uri) as any;
+    try {
+        // 使用vscode.workspace.fs.readDirectory来获取目录内容
+        const entries = await vscode.workspace.fs.readDirectory(uri) as any;
 
-      let map = {} as any
-      entries.map((entry: string[]) => {
-            if(entry[0].includes('css')){
-              map['css'] = vscode.Uri.joinPath(uri, entry[0])
+        let map = {} as any
+        entries.map((entry: string[]) => {
+            if (entry[0].includes('css')) {
+                map['css'] = vscode.Uri.joinPath(uri, entry[0])
             }
-            if(entry[0].includes('js')){
-              map['js'] = vscode.Uri.joinPath(uri, entry[0])
+            if (entry[0].includes('js')) {
+                map['js'] = vscode.Uri.joinPath(uri, entry[0])
             }
-          }); 
-      
-      return map;
-  } catch (error) {
-      console.error(`Failed to read directory: ${error}`);
-      return [];
-  }
+        });
+
+        return map;
+    } catch (error) {
+        console.error(`Failed to read directory: ${error}`);
+        return [];
+    }
 }
 
 async function getWebviewHTML(panel: vscode.WebviewPanel, extensionUri: Uri): Promise<string> {
-  let res = await listFilesInDirectory(vscode.Uri.joinPath(extensionUri, 'out', 'view', 'assets'))
-  // js地址动态转换
-  const scriptUri = panel.webview.asWebviewUri(res['js']);
-  // css地址动态转换
-  const styleUri = panel.webview.asWebviewUri(res['css']);
+    let res = await listFilesInDirectory(vscode.Uri.joinPath(extensionUri, 'out', 'view', 'assets'))
+    // js地址动态转换
+    const scriptUri = panel.webview.asWebviewUri(res['js']);
+    // css地址动态转换
+    const styleUri = panel.webview.asWebviewUri(res['css']);
 
-  return `<!DOCTYPE html>
+    return `<!DOCTYPE html>
   <html lang="en">
     <head>
       <meta charset="UTF-8">
--- packages/view/src/App.vue
+++ packages/view/src/App.vue
@@ -1,18 +1,39 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 const aa = ref("")
-
+declare let  acquireVsCodeApi: any;
+interface ResultData {
+    message:String
+}
+const vscode = acquireVsCodeApi()
+let  eventData = reactive({message:''})
 const confirm = () => {
   // 请求后端接口
   console.log(aa.value)
+  vscode.postMessage({ type: 'hostAdd',data:'hostAdd' });
 }
+//监听插件消息
+window.addEventListener('message', event => {
+    // 确保消息来源是可信的，通常是检查event.origin
+    // vscode.postMessage({ message: 'hostAdd',data:event });
+    console.log(JSON.parse(event.data));
+    
+    // if (event.origin !== 'yourExtensionOrigin') return;
+    // alert(event)
+    // const message = event.data;
+    // console.log('Received message:', message);
+
+    // if (message.command === 'yourCommand') {
+    //     console.log(11);
+    // }
+});
 </script>
 
 <template>
   <div>
     <el-input v-model="aa" placeholder="主机名"></el-input>
-
+    {{ eventData }} {{ eventData.message }}
     <el-button @click="confirm">确定</el-button>
   </div>
 </template>
